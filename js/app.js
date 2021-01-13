f8b53b2e312d0cf34e2c029593f4efab6bb91918
Qadree Trimble <qadree_t@yahoo.com>
2021-01-12 21:47:22 -0800

    adding content for the assignment and additonal hidden files

---
+++ js/app.js
@@ -0,0 +1,8 @@
+'use strict'
+
+//write 5 questions, must except yes/no or y/n.
+//examples: yes, YES, Yes, y or Y
+
+var userName = prompt('Hello What is your name?');
+console.log(userName);
+alert('What as wonderful name! ' + userName + 'It\'s such a pleasure to know your name! ');
