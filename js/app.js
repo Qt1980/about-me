67fb90ebaee4172be327f315a7f13f91afe1933e
Qadree Trimble <qadree_t@yahoo.com>
2021-01-13 21:01:40 -0800

    commented java back in.

--- js/app.js
+++ js/app.js
@@ -1,70 +1,70 @@
-// //'use strict'
+//'use strict'
 
-// //write 5 questions, must except yes/no or y/n.
-// //examples: yes, YES, Yes, y or Y
+//write 5 questions, must except yes/no or y/n.
+//examples: yes, YES, Yes, y or Y
 
-// //introduction
-// var userName = prompt('Hello What is your name?');
-// //console.log(userName);
-// alert('What as wonderful name! ' + userName + ', It\'s such a pleasure to meet you! ');
+//introduction
+var userName = prompt('Hello What is your name?');
+//console.log(userName);
+alert('What as wonderful name! ' + userName + ', It\'s such a pleasure to meet you! ');
 
-// //first question
-// var homeCity = prompt('So, Let\'s get to know each other! Are you from the planet Earth?').toLowerCase();
+//first question
+var homeCity = prompt('So, Let\'s get to know each other! Are you from the planet Earth?').toLowerCase();
 
-// //console.log(homeCity);
+//console.log(homeCity);
 
-// if(homeCity === 'yes' || homeCity === 'y') {
-//   alert('Yes! Yes you are otherwise I would call...Nevermind!');
-// } else if (homeCity === 'no' || homeCity === 'n') {
+if(homeCity === 'yes' || homeCity === 'y') {
+  alert('Yes! Yes you are otherwise I would call...Nevermind!');
+} else if (homeCity === 'no' || homeCity === 'n') {
 
-//   alert('Ah well that\'s ok too. Most Jedi are familiar with various Alien species.');
-// }
-
-
-
-// //second question
-
-// var stayingAlive = prompt('Do you enjoy meditating?').toLowerCase();
-
-// //console.log(stayingAlive);
-
-// if(stayingAlive === 'yes' || stayingAlive === 'y') {
-//   alert('Very Good Padawan! Meditation leads to strength in he force!');
-// }else if (stayingAlive === 'no' || stayingAlive === 'n') {
-//   alert('Hmm, If you do not then to the dark side you will go!');
-// }
+  alert('Ah well that\'s ok too. Most Jedi are familiar with various Alien species.');
+}
 
 
-// // third question
 
-// var angryAntics = prompt('Do you like to be angry?').toLowerCase();
-//  //console.log(angryAntics);
+//second question
 
-// if(angryAntics === 'yes' || angryAntics === 'y'){
-//   alert('The darkside of the force calls to you. You must learn to quell your emotions.');
-// }else if (angryAntics === 'no' || angryAntics === 'n') {
-//   alert('Very Good! This is the true path of the Jedi!');
-// }
+var stayingAlive = prompt('Do you enjoy meditating?').toLowerCase();
 
-// //fourth question
+//console.log(stayingAlive);
 
-// var lightSaber = prompt('Do you possess a lightsaber?').toLowerCase();
+if(stayingAlive === 'yes' || stayingAlive === 'y') {
+  alert('Very Good Padawan! Meditation leads to strength in he force!');
+}else if (stayingAlive === 'no' || stayingAlive === 'n') {
+  alert('Hmm, If you do not then to the dark side you will go!');
+}
 
-// if(lightSaber === 'yes' || lightSaber === 'y'){
-//   //console.log(lightSaber);
-//   alert('Wonderful! A saber is a Jedi\'s weapon and it is connected to you through the force.');
-// }else if (lightSaber === 'no' || lightSaber === 'n') {
-//   alert('Hmph! Then you must be a child of darkness!');
-// }
 
-// //Fifth question
+// third question
 
-// var oneForce = prompt('Are you one with the force?').toLowerCase();
+var angryAntics = prompt('Do you like to be angry?').toLowerCase();
+ //console.log(angryAntics);
 
-// //console.log(oneForce);
+if(angryAntics === 'yes' || angryAntics === 'y'){
+  alert('The darkside of the force calls to you. You must learn to quell your emotions.');
+}else if (angryAntics === 'no' || angryAntics === 'n') {
+  alert('Very Good! This is the true path of the Jedi!');
+}
 
-// if(oneForce === 'yes' || oneForce === 'y'){
-//   alert('This is good ' + userName + ', The force is strong with you!');
-// }else if (oneForce === 'no' || oneForce === 'n') {
-//   alert(userName +' you are a Sith Lord. Prepare to do battle!');
-// }
+//fourth question
+
+var lightSaber = prompt('Do you possess a lightsaber?').toLowerCase();
+
+if(lightSaber === 'yes' || lightSaber === 'y'){
+  //console.log(lightSaber);
+  alert('Wonderful! A saber is a Jedi\'s weapon and it is connected to you through the force.');
+}else if (lightSaber === 'no' || lightSaber === 'n') {
+  alert('Hmph! Then you must be a child of darkness!');
+}
+
+//Fifth question
+
+var oneForce = prompt('Are you one with the force?').toLowerCase();
+
+//console.log(oneForce);
+
+if(oneForce === 'yes' || oneForce === 'y'){
+  alert('This is good ' + userName + ', The force is strong with you!');
+}else if (oneForce === 'no' || oneForce === 'n') {
+  alert(userName +' you are a Sith Lord. Prepare to do battle!');
+}
