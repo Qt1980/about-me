d9f190900c86ea9438df60f774923729e1adabc0
Qadree Trimble <qadree_t@yahoo.com>
2021-01-16 18:10:31 -0800

    added the last two questions in about me page

--- js/app.js
+++ js/app.js
@@ -1,83 +1,109 @@
 'use strict';
 
-// //write 5 questions, must except yes/no or y/n.
-// //examples: yes, YES, Yes, y or Y
+var score = 0;
 
-// //introduction
-// var userName = prompt('Hello What is your name?');
-// //console.log(userName);
-// alert('What as wonderful name! ' + userName + ', It\'s such a pleasure to meet you! ');
+//write 5 questions, must except yes/no or y/n.
+//examples: yes, YES, Yes, y or Y
 
-// //first question
-// var homeCity = prompt('So, Let\'s get to know each other! Are you from the planet Earth?').toLowerCase();
+//introduction
+var userName = prompt('Hello What is your name?');
+//console.log(userName);
+alert('What as wonderful name! ' + userName + ', It\'s such a pleasure to meet you! ');
 
-// //console.log(homeCity);
+//first question
+var homeCity = prompt('So, Let\'s get to know each other! Are you from the planet Earth?').toLowerCase();
 
-// if(homeCity === 'yes' || homeCity === 'y') {
-//   alert('Yes! Yes you are otherwise I would call...Nevermind!');
-// } else if (homeCity === 'no' || homeCity === 'n') {
+//console.log(homeCity);
 
-//   alert('Ah well that\'s ok too. Most Jedi are familiar with various Alien species.');
-// }
+if(homeCity === 'yes' || homeCity === 'y') {
+  alert('Yes! Yes you are otherwise I would call...Nevermind!');
+} else if (homeCity === 'no' || homeCity === 'n') {
 
-// //second question
+  alert('Ah well that\'s ok too. Most Jedi are familiar with various Alien species.');
+}
 
-// var stayingAlive = prompt('Do you enjoy meditating?').toLowerCase();
+//second question
 
-// //console.log(stayingAlive);
+var stayingAlive = prompt('Do you enjoy meditating?').toLowerCase();
 
-// if(stayingAlive === 'yes' || stayingAlive === 'y') {
-//   alert('Very Good Padawan! Meditation leads to strength in he force!');
-// }else if (stayingAlive === 'no' || stayingAlive === 'n') {
-//   alert('Hmm, If you do not then to the dark side you will go!');
-// }
+//console.log(stayingAlive);
 
-// // third question
+if(stayingAlive === 'yes' || stayingAlive === 'y') {
+  alert('Very Good Padawan! Meditation leads to strength in he force!');
+}else if (stayingAlive === 'no' || stayingAlive === 'n') {
+  alert('Hmm, If you do not then to the dark side you will go!');
+}
 
-// var angryAntics = prompt('Do you like to be angry?').toLowerCase();
-//  //console.log(angryAntics);
+// third question
 
-// if(angryAntics === 'yes' || angryAntics === 'y'){
-//   alert('The darkside of the force calls to you. You must learn to quell your emotions.');
-// }else if (angryAntics === 'no' || angryAntics === 'n') {
-//   alert('Very Good! This is the true path of the Jedi!');
-// }
+var angryAntics = prompt('Do you like to be angry?').toLowerCase();
+//console.log(angryAntics);
 
-// //fourth question
+if(angryAntics === 'yes' || angryAntics === 'y'){
+  alert('The darkside of the force calls to you. You must learn to quell your emotions.');
+}else if (angryAntics === 'no' || angryAntics === 'n') {
+  alert('Very Good! This is the true path of the Jedi!');
+}
 
-// var lightSaber = prompt('Do you possess a lightsaber?').toLowerCase();
+//fourth question
 
-// if(lightSaber === 'yes' || lightSaber === 'y'){
-//   //console.log(lightSaber);
-//   alert('Wonderful! A saber is a Jedi\'s weapon and it is connected to you through the force.');
-// }else if (lightSaber === 'no' || lightSaber === 'n') {
-//   alert('Hmph! Then you must be a child of darkness!');
-// }
+var lightSaber = prompt('Do you possess a lightsaber?').toLowerCase();
 
-// //Fifth question
+if(lightSaber === 'yes' || lightSaber === 'y') {
+  //console.log(lightSaber);
+  alert('Wonderful! A saber is a Jedi\'s weapon and it is connected to you through the force.');
+  score++;
+}else if (lightSaber === 'no' || lightSaber === 'n') {
+  alert('Hmph! Then you must be a child of darkness!');
+}
 
-// var oneForce = prompt('Are you one with the force?').toLowerCase();
+//Fifth question
 
-// //console.log(oneForce);
+var oneForce = prompt('Are you one with the force?').toLowerCase();
 
-// if(oneForce === 'yes' || oneForce === 'y'){
-//   alert('This is good ' + userName + ', The force is strong with you!');
-// }else if (oneForce === 'no' || oneForce === 'n') {
-//   alert(userName +' you are a Sith Lord. Prepare to do battle!');
-// }
+//console.log(oneForce);
+
+if(oneForce === 'yes' || oneForce === 'y'){
+  alert('This is good ' + userName + ', The force is strong with you!');
+  score++;
+}else if (oneForce === 'no' || oneForce === 'n') {
+  alert(userName +' you are a Sith Lord. Prepare to do battle!');
+}
 
 for (var i = 0; i < 4; i++) {
-var guessmyAge = +prompt('How old do you think I am?');
-console.log(typeof(guessmyAge));
-var age = 40;
-if (guessmyAge === age) {
-  alert('Bingo! You guessed it!');
-break;
-}else if (i < 3 && guessmyAge < age) {
+  var guessmyAge = +prompt('How old do you think I am?');
+  //console.log(typeof(guessmyAge));
+  var age = 40;
+  if (guessmyAge === age) {
+    alert('Bingo! You guessed it!');
+    score++;
+    break;
+  } else if (i < 3 && guessmyAge < age) {
     alert('Nice try but you need to go a bit higher.');
-}else if (i < 3 && guessmyAge > age) {
+  } else if (i < 3 && guessmyAge > age) {
     alert('Ah not that old! Lower please!');
-}else if (i === 3) {
+  } else if (i === 3) {
     alert('Sorry your out of guesses. I am 40.');
+  }
 }
+
+
+var answerCorrectly = false;
+var myfavoriteAnimals = ['House cat', 'Golden Eagle', 'Bat', 'Dog', 'Octopus', 'Snake', 'Scorpion'];
+//console.log(myfavoriteAnimals);
+for (var j = 0; j < 6; j++) {
+  var guessMyfavorites = prompt('What are my favorite animals');
+  for (var k = 0; k < myfavoriteAnimals.length; k++) {
+    if (guessMyfavorites === myfavoriteAnimals[k]) {
+      alert('Correct');
+      answerCorrectly = true;
+      score++;
+    }
+  }
+  if (answerCorrectly) {
+    break;
+  }
 }
+alert('My favorite animals are House cat, Golden Eagle, Bat, Dog, Octopus, Snake Scorpion');
+console.log(score);
+alert( 'Good job' + userName + 'You scored well')
